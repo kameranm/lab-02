91881b7dd776a8f171a0d4f440ef910acef58362
kameranm <123990742+kameranm@users.noreply.github.com>
2023-02-06 19:23:30 -0800

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -1,6 +1,10 @@
 /* A simple server in the internet domain using TCP
  * Answer the questions below in your writeup
  */
+
+/* A simple server in the internet domain using TCP
+ * Answer the questions below in your writeup
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,18 +21,18 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    /* 1. What is argc and *argv[]?
-     *
+/* What is argc and *argv[]?
+Argc is an integer that represents how many arguments are in the command line while *argv[] is simply the name of the program that is being executed.
      */
     int sockfd, newsockfd, portno;
     /* 2. What is a UNIX file descriptor and file descriptor table?
-     *
+A UNIX file descriptor is essentially an input output stream that uses integer inputs that connect to the file descriptor table that are seen in the stream such as stdin, or stderr. Applications can change file descriptors to access the descriptor table and the object array. 
      */
     socklen_t clilen;
 
     struct sockaddr_in serv_addr, cli_addr;
     /* 3. What is a struct? What's the structure of sockaddr_in?
-     *
+    A struct is a type of data declaration that groups different variables into one place. The structure of sockaddr_in is used to store addresses for servers in internet connections. 
      */
     
     int n;
@@ -39,7 +43,7 @@ int main(int argc, char *argv[])
     
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     /* 4. What are the input parameters and return value of socket()
-     *
+The input parameters of socket() are the domain which is an integer used to specify the communication domain, the communication type (ex: Sock stream which has to do with TCP communication), and the protocol value for IP which will most of the time be 0 which signals it has gone through. If it is -1, it has an error. 
      */
     
     if (sockfd < 0) 
@@ -54,7 +58,8 @@ int main(int argc, char *argv[])
              sizeof(serv_addr)) < 0) 
              error("ERROR on binding");
     /* 5. What are the input parameters of bind() and listen()?
-     *
+    The input parameters of bind() are the descriptor of a socket, the pointer used for the address of a socket, and the size of a socket address.
+The input parameters of listen() are the socket descriptor returned by the socket function and the number of connections allowed. 
      */
     
     listen(sockfd,5);
@@ -62,7 +67,7 @@ int main(int argc, char *argv[])
     
     while(1) {
         /* 6.  Why use while(1)? Based on the code below, what problems might occur if there are multiple simultaneous connections to handle?
-        *
+While(1) is an infinite Loop which runs until there is a break so in terms of the serer, it is used to constantly receive data from the clients. If there are multiple connections to hande simultaneously, it may send the wrong return data packets to aclient and the timing would be slower. 
         */
         
 	char buffer[256];
@@ -70,7 +75,7 @@ int main(int argc, char *argv[])
                     (struct sockaddr *) &cli_addr, 
                     &clilen);
 	/* 7. Research how the command fork() works. How can it be applied here to better handle multiple connections?
-         * 
+         * The command fork() can help handle multiple clients because it creates a child process that runs in sync with the parent process which runs concurrently with each other allowing for multiple clients at the same time without losing efficiency. 
          */
         
 	if (newsockfd < 0) 
@@ -80,7 +85,7 @@ int main(int argc, char *argv[])
 	n = read(newsockfd,buffer,255);
         if (n < 0) 
             error("ERROR reading from socket");
-        //printf("Here is the message: %s\n",buffer);
+        printf("Here is the message: %s\n",buffer);
         n = write(newsockfd,"I got your message",18);
         if (n < 0) 
             error("ERROR writing to socket");
@@ -91,5 +96,4 @@ int main(int argc, char *argv[])
 }
   
 /* This program makes several system calls such as 'bind', and 'listen.' What exactly is a system call?
- *
- */
+A system call is the way a computer makes a request for an action from the operating system where it is conducted such as bind() and listen().
