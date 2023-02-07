5ba11f6bce3527dbe9bf7678f4e4f3ba478b147a
kameranm <123990742+kameranm@users.noreply.github.com>
2023-02-06 19:22:45 -0800

    Update tcp_client.cpp

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -1,5 +1,7 @@
 // Server side C/C++ program to demonstrate Socket programming 
 // Here's some include statements that might be helpful for you
+// Server side C/C++ program to demonstrate Socket programming 
+// Here's some include statements that might be helpful for you
 #include <string> 
 #include <cstring>
 #include <iostream>
@@ -16,13 +18,15 @@ int main(int argc, char const *argv[])
 	char socket_read_buffer[1024];
 	
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+	std::string server_ip = "0.0.48.57"; // 
+	std::string server_port = "12345"; // whicher port I wanted above 8000
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
+
+    client_fd = socket(AF_INET, SOCK_STREAM, 0); // from sample code on web linked
 
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
@@ -34,7 +38,7 @@ int main(int argc, char const *argv[])
 		printf("Error- Socket setup failed");
 		return -1;
 	}
-	
+
 	// Helping you out by pepping the struct for connecting to the server
 	struct addrinfo hints;
 	struct addrinfo *server_addr;
@@ -44,10 +48,25 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
-	// TODO: Retreive user input
-	// TODO: Send() the user input to the server
-	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
-	// TODO: Close() the socket
+    if (connect(client_fd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) { // syntax from client link 
+		printf("ERROR Connecting");
+	}
+    // TODO: Retreive user input
+    printf("Please enter the message: "); // taken from client link
+    bzero(socket_read_buffer,1024); 
+    fgets(socket_read_buffer,1023,stdin);
+
+    // TODO: Send() the user input to the server
+    send(client_fd, socket_read_buffer, 1024, 0);
+	
+    // TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
+	bzero(socket_read_buffer, 1024); // code also from client link 
+    read(client_fd, socket_read_buffer, 1024);
+    printf("Message from server: %s", socket_read_buffer);
+
+
+    // TODO: Close() the socket
+    close(client_fd);
 
 	return 0; 
 } 
